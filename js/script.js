04540224f348694d8a6eb14ee0e74fbe99d94ead
Darrion Gering and Gavin Kimberlin <gavin@>
2019-03-25 16:14:08 -0700

    add a message displaying the number of characters in the sentence that was typed by the user.

--- js/script.js
+++ js/script.js
@@ -5,9 +5,22 @@ var firstLast = function(sentence) {
   return (first + last);
 }
 
+var reverseOrder = function(sentence) {
+  var first = sentence.charAt(0).toUpperCase();
+  var length = sentence.length;
+  var last = sentence.charAt(length - 1).toUpperCase();
+  alert(length);
+  return (last + first);
 
+}
 
+var printResult = function(sentence) {
+  var reverse = reverseOrder(sentence)
+  var forward = firstLast(sentence)
+  alert(reverse);
+  alert(forward);
+}
 
 var sentence = prompt("Enter a sentence: ");
 
-sentence = (firstLast(sentence));
+printResult(sentence)
