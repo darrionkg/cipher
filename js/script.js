9f7d7d6f927d7e3ca724c0a7054e6196d1ce4ea8
Darrion Gering and Gavin Kimberlin <darrion@>
2019-03-25 16:51:17 -0700

    Add getLength function

--- js/script.js
+++ js/script.js
@@ -3,24 +3,30 @@ var firstLast = function(sentence) {
   var length = sentence.length;
   var last = sentence.charAt(length - 1).toUpperCase();
   return (first + last);
-}
+};
 
 var reverseOrder = function(sentence) {
   var first = sentence.charAt(0).toUpperCase();
   var length = sentence.length;
   var last = sentence.charAt(length - 1).toUpperCase();
-  alert(length);
   return (last + first);
-
-}
+};
 
 var printResult = function(sentence) {
-  var reverse = reverseOrder(sentence)
-  var forward = firstLast(sentence)
-  alert(reverse);
-  alert(forward);
-}
+  var reverse = reverseOrder(sentence);
+  var forward = firstLast(sentence);
+  return(sentence + reverse);
+};
+
+var getLength = function(sentence, sentence2) {
+  var length = sentence.length;
+  var half = parseFloat((length / 2) - .5);
+  var halfLetter = sentence.charAt(half);
+  alert(halfLetter + sentence2);
+};
 
 var sentence = prompt("Enter a sentence: ");
 
-printResult(sentence)
+var sentence2 = printResult(sentence);
+
+getLength(sentence, sentence2);
